b784d1d3ebab26f8c8c1aee0cd1e4caff5ad8408
Hay-sharptech <you@example.com>
2021-08-14 23:25:24 -0700

    assignment mappings

--- mappings.sol
+++ mappings.sol
@@ -1,8 +1,15 @@
 pragma solidity 0.8.0;
+
+// A contract contains two very important mappings which are used to track some very *private* info...
+// The first mapping allows you to know how many times an address has called a function,
+// the second one allows you to know the function hash that the address called last...
+// Write three different functions that don't *really do anything but returns a bool value*
+// and update the mappings accordingly
+
 contract Mappyn{     
-    mapping(address => uint) private addressToInt;
-    mapping(address => bytes32) private addressToHashes;
-    function 1stfirst() public returns(bool){
+    mapping(address => uint) public addressToInt;
+    mapping(address => bytes32) public addressToHashes;
+    function first() public returns(bool){
         addressToInt[msg.sender]++;
         addressToHashes[msg.sender] = keccak256(abi.encodeWithSignature("1stfirst()"));
         return true;
@@ -12,16 +19,11 @@ contract Mappyn{
          addressToHashes[msg.sender] = keccak256(abi.encodeWithSignature("second()"));
         return true;
     }
-     function third() public returns(bool){
-         addressToInt[msg.sender]++;
-         addressToHashes[msg.sender] = keccak256(abi.encodeWithSignature("third()"));
-        return true;
-    }
-
+    
    
- function 3rdthird() public returns(bool){
-         addressToHashes[msg.sender] = keccak256(abi.encodeWithSignature("3rdthird()"));
-         addresstofInt[msg.sender]++;
+ function tthird() public returns(bool){
+         addressToHashes[msg.sender] = keccak256(abi.encodeWithSignature("tthird()"));
+         addressToInt[msg.sender]++;
         return true;
     }
 }
